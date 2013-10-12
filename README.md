93bd4be80defee6d5d8df4bccac035bacfeea09a
houxianxu <hxianxu@gmail.com>
2013-10-12 14:09:49 +0800

    add all the file

---
+++ README.md
@@ -0,0 +1,6 @@
+# node-js-sample
+
+This is the code of my personal website. And deployed with [heroku](https://heroku.com/)
+
+http://houxianxu.herokuapp.com/
+
---
+++ web.js
@@ -0,0 +1,31 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , http = require('http')
+  , path = require('path')
+  , fs = require('fs');
+
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.use(express.static('public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', function (req, res) {
+  res.send(fs.readFileSync("./index.html", "utf8").toString());
+});
+
+
+
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
